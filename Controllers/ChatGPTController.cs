72dcb853724d334573ce0b47ab0895de5eebcfcb
Rodrigo Zubieta <ext.rodrigo.zubieta@pricetravel.com>
2023-07-31 16:07:28 -0400

    add chatgpt

---
+++ Controllers/ChatGPTController.cs
@@ -0,0 +1,59 @@
+﻿using DemoChatGPT.Models.ChatGPT;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DemoChatGPT.Controllers
+{
+    public class ChatGPTController : Controller
+    {
+        public static string _Endpoint = "https://api.openai.com/";
+        public static string _URI = "v1/chat/completions";
+        public static string _ApiKey = "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";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string request)
+        {
+            try
+            {
+                var client = new RestClient(_Endpoint);
+                var requestOpenIA = new RestRequest(_URI);
+
+                requestOpenIA.AddHeader("Content-Type", "application/json");
+                requestOpenIA.AddHeader("Authorization", $"Bearer {_ApiKey}");
+
+                var body = new Request
+                {
+                    model = "gpt-3.5-turbo",
+                    messages = new List<Message>() {
+                    new Message
+                    {
+                        role = "user",
+                        content = request
+                    }
+                }
+                };
+
+                var jsonBody = JsonSerializer.Serialize(body);
+                requestOpenIA.AddJsonBody(jsonBody);
+
+                var responseOpenIA = client.Post<Response>(requestOpenIA);
+                var response = responseOpenIA.choices[0].message.content;
+
+                ViewBag.Response = response;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Response = ex;
+            }
+          
+            return View();
+        }
+    }
+}
---
+++ Models/ChatGPT/Request.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoChatGPT.Models.ChatGPT
+{
+
+    public class Request
+    {
+        public string model { get; set; }
+        public List<Message> messages { get; set; }
+    }
+
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+
+}
